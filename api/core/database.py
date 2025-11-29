3ea4cc7c897110500cc6b52d53ddfa261d3cf395
Ziadashraf301 <ziadashraf98765@gmail.com>
2025-11-29 14:09:48 +0200

    fix race condition

--- api/core/database.py
+++ api/core/database.py
@@ -10,9 +10,11 @@ from sqlalchemy import (
 from datetime import datetime, timedelta
 from typing import Dict, List
 import uuid
-
+from sqlalchemy.exc import IntegrityError
+import logging
 from config import settings
 
+logger = logging.getLogger(__name__)
 Base = declarative_base()
 
 
